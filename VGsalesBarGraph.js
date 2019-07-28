dd4b8b14f4f72030e2ace4f42a3a7fc46e081dbf
DRP <danielp6@illinois.edu>
2019-07-28 11:29:36 -0500

    m

---
+++ VGsalesBarGraph.js
@@ -0,0 +1,36 @@
+
+var margin = {top: 20, right: 10, bottom: 100, left:50},
+    width = 700 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("body")
+    .append("svg")
+      .attr ({
+        "width": width + margin.right + margin.left,
+        "height": height + margin.top + margin.bottom
+      })
+    .append("g")
+      .attr("transform","translate(" + margin.left + "," + margin.right + ")");
+
+var xScale = d3.scale.ordinal()
+    .rangeRoundBands([0,width], 0.2, 0.2);
+
+var yScale = d3.scale.linear()
+    .range([height, 0]);
+
+// define x and y axis
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+d3.csv("VGM.csv", function(error,data) {
+  if(error) console.log("Error loading data");
+
+  data.forEach(function(d) {
+    d.name = d.name
+    console.log(d.name);
+})});
