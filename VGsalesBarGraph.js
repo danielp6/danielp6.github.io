f49a9a7b78d482aa806e5ba5d6c04281319779de
DRP <danielp6@illinois.edu>
2019-07-28 13:41:18 -0500

    m

--- VGsalesBarGraph.js
+++ VGsalesBarGraph.js
@@ -3,14 +3,14 @@ var margin = {top: 20, right: 10, bottom: 100, left:50},
     width = 700 - margin.right - margin.left,
     height = 500 - margin.top - margin.bottom;
 
-// var svg = d3.select("body")
-//     .append("svg")
-//       .attr ({
-//         "width": width + margin.right + margin.left,
-//         "height": height + margin.top + margin.bottom
-//       })
-//     .append("g")
-//       .attr("transform","translate(" + margin.left + "," + margin.right + ")");
+var svg = d3.select("body")
+    .append("svg")
+      .attr ({
+        "width": width + margin.right + margin.left,
+        "height": height + margin.top + margin.bottom
+      })
+    .append("g")
+      .attr("transform","translate(" + margin.left + "," + margin.right + ")");
 
 var xScale = d3.scale.ordinal()
     .rangeRoundBands([0,width], 0.2, 0.2);
