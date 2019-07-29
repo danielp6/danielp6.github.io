5a615de3afdc48525e475b2a672e1cf469ccc296
DRP <danielp6@illinois.edu>
2019-07-29 18:24:28 -0500

    m

--- manu.js
+++ manu.js
@@ -1,19 +1,23 @@
-
-var margin = {top: 20, right: 10, bottom: 100, left:100},
+var margin = {
+        top: 20,
+        right: 10,
+        bottom: 100,
+        left: 100
+    },
     width = 700 - margin.right - margin.left,
     height = 500 - margin.top - margin.bottom;
 
 var svg = d3.select("body")
     .append("svg")
-      .attr ({
+    .attr({
         "width": width + margin.right + margin.left,
         "height": height + margin.top + margin.bottom
-      })
+    })
     .append("g")
-      .attr("transform","translate(" + margin.left + "," + margin.right + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
 
 var xScale = d3.scale.ordinal()
-    .rangeRoundBands([0,width], 0.1);
+    .rangeRoundBands([0, width], 0,0);
 
 var yScale = d3.scale.linear()
     .range([height, 0]);
@@ -26,51 +30,67 @@ var yAxis = d3.svg.axis()
     .scale(yScale)
     .orient("left");
 
-    d3.csv("VGM-3.csv", function(error, csv_data) {
-     var data = d3.nest()
-      .key(function(d) {if (! (Number.isNaN(d.key)|| d.key == ""))  return d.Manufacturer;})
-      .rollup(function(d) {
-       return d3.sum(d, function(g) {return g.Global_Sales; });
-      }).entries(csv_data);
+d3.csv("VGM-3.csv", function(error, csv_data) {
+    var data = d3.nest()
+        .key(function(d) {
+            return d.Manufacturer;
+        })
+        .rollup(function(d) {
+            return d3.sum(d, function(g) {
+                return g.Global_Sales;
+            });
+        }).entries(csv_data);
 
-data.forEach(function(d) {
- d.Manufacturer = d.key;
- d.Global_Sales = d.values;
-})
+    data.forEach(function(d) {
+        d.Manufacturer = d.key;
+        d.Global_Sales = d.values;
+    })
 
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(function(d) {
-    return "<strong>Sales:</strong> <span style='color:red'>"+ (d.Global_Sales).toFixed(2) + "</span>";
-});
- svg.call(tip);
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "<strong>Sales:</strong> <span style='color:red'>" + (d.Global_Sales).toFixed(2) + "</span>";
+        });
+    svg.call(tip);
 
-xScale.domain(data.map(function(d) { return d.Manufacturer; }) );
-yScale.domain([0, d3.max(data, function(d) { return d.Global_Sales; } ) ]);
+    xScale.domain(data.map(function(d) {
+        return d.Manufacturer;
+    }));
+    yScale.domain([0, d3.max(data, function(d) {
+        return d.Global_Sales;
+    })]);
 
-svg.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append('rect')
-	.attr("class","bar")
-    .attr("height", 0)
-    .attr("y", height)
-	.on('mouseover', tip.show)
-    .on('mouseout', tip.hide)
-    .transition().duration(2500)
-    .delay( function(d,i) { return i * 200; })
-    .attr({
-      "x": function(d) { return xScale(d.Manufacturer); },
-      "y": function(d) { return yScale(d.Global_Sales); },
-      "width": xScale.rangeBand(),
-      "height": function(d) { return  height - yScale(d.Global_Sales); }
-  });
+    svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr("class", "bar")
+        .attr("height", 0)
+        .attr("y", height)
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+        .transition().duration(2500)
+        .delay(function(d, i) {
+            return i * 200;
+        })
+        .attr({
+            "x": function(d) {
+                return xScale(d.Manufacturer);
+            },
+            "y": function(d) {
+                return yScale(d.Global_Sales);
+            },
+            "width": xScale.rangeBand(),
+            "height": function(d) {
+                return height - yScale(d.Global_Sales);
+            }
+        });
 
-        svg.selectAll('text')
-            .data(data)
-            .enter()
-            .append('text')
+    svg.selectAll('text')
+        .data(data)
+        .enter()
+        .append('text')
 
     svg.append("g")
         .attr("class", "x axis")
@@ -79,7 +99,7 @@ svg.selectAll(".bar")
         .selectAll("text")
         .attr("dx", "-.8em")
         .attr("dy", ".25em")
-        .attr("transform", "rotate(-60)" )
+        .attr("transform", "rotate(-60)")
         .style("text-anchor", "end")
         .attr("font-size", "10px");
 
@@ -88,8 +108,8 @@ svg.selectAll(".bar")
         .call(yAxis)
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("x", -height/2)
-		.attr("y", -50)
+        .attr("x", -height / 2)
+        .attr("y", -50)
         .attr("dy", "-3em")
         .style("text-anchor", "middle")
         .text("Global sales in Millions of Dollars");
