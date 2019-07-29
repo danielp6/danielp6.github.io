9a7d08dfed8c0f84d584877bd340a636e4ad16ce
DRP <danielp6@illinois.edu>
2019-07-29 15:17:18 -0500

    m

--- manu.js
+++ manu.js
@@ -1,6 +1,6 @@
 
 var margin = {top: 20, right: 10, bottom: 100, left:50},
-    width = 800 - margin.right - margin.left,
+    width = 600 - margin.right - margin.left,
     height = 500 - margin.top - margin.bottom;
 
 var svg = d3.select("body")
