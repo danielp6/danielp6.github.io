a9ba115ea2830bb26b9301c8f0be0ce22215b37c
DRP <danielp6@illinois.edu>
2019-07-29 18:57:20 -0500

    m

--- manu.js
+++ manu.js
@@ -54,7 +54,7 @@ d3.csv("VGM-3.csv", function(error, csv_data) {
         });
     svg.call(tip);
 
-    xScale.domain(["Nintendo", "PC" ]);// {
+    xScale.domain(d.Manufacturer.map());// {
         //return d.Manufacturer
     //}));
     yScale.domain([0, d3.max(data, function(d) {
