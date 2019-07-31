b17227a56de4da14774b48311192a5f15d2db9d0
DRP <danielp6@illinois.edu>
2019-07-31 09:15:04 -0500

    slider

--- consoles.js
+++ consoles.js
@@ -1,6 +1,16 @@
-function updateData(year) {
-d3.select("svg").remove();
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
 
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+updateData(this.value);
+}
+
+
+function updateData(year) {
+
+d3.select("svg").remove();
 var margin = {
     top: 20,
     right: 10,
