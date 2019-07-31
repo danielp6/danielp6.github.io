71956b66730fdd2bb0ed8ed8eeb11a6296d2ce38
DRP <danielp6@illinois.edu>
2019-07-31 09:53:29 -0500

    slider

--- consoles.js
+++ consoles.js
@@ -5,6 +5,7 @@ var slider = document.getElementById("myRange");
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
 console.log(slider.value);
+updateData(slider.value);
 }
 
 
